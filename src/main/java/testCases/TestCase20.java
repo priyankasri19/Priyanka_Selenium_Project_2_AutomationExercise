9343bdd169d13784199992f2892d682a3e92ba31
Priyanka Srivastava <priyanka.19aug@gmail.com>
2023-11-16 17:48:16 +0530

    Project 2 changes

--- src/main/java/testCases/TestCase20.java
+++ src/main/java/testCases/TestCase20.java
@@ -1,53 +1,65 @@
 package testCases;
 
 import java.io.IOException;
-import java.text.ParseException;
+import org.json.simple.parser.ParseException;
 import java.util.List;
 
+import org.json.simple.JSONObject;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import pages.CartPage;
 import pages.HomePage;
-import pages.JSONReader;
 import pages.ProductsPage;
+import setup.TestDataReader;
+import setup.TestSetup;
 
 public class TestCase20 extends TestSetup {
-
-	   public void searchProductsAndVerifyCartAfterLogin() throws IOException, ParseException, InterruptedException {
-	        TestCase8.verifyUserIsNavigatedToAllProductsPageSuccessfully();
-	        TestCase9.verifySearchedProductsIsVisible();
-	        List<String> productsNames = TestCase9.verifyAllTheProductsRelatedToSearchAreVisible();
-	        new ProductsPage(getDriver()).addAllProducts();
-	        clickCartButtonAndVerifyThatProductsAreVisibleInCart(productsNames);
-	        new HomePage(getDriver())
+	
+	@Test
+	public void searchProductsAndVerifyCartAfterLogin() throws IOException, InterruptedException, ParseException {
+		TestCase8.checkIsUserNavigatedToAllProductsPage();
+	    TestCase9.isSearchedProductsVisible();
+	    List<String> productsNames = TestCase9.checkIsAllTheProductsRelatedToSearchAreVisible();
+	    new ProductsPage(getDriver()).addAllProducts();
+	    clickCartButtonAndVerifyThatProductsAreVisibleInCart(productsNames);
+	    
+    	JSONObject validUserDetails = TestDataReader.validUserDetails();
+		String email=(String) validUserDetails.get("email");
+		String password=(String) validUserDetails.get("password");
+		
+	    new HomePage(getDriver())
 	                .signupLoginClick()
-	                .fillCorrectLogin(JSONReader.existingUser("email"), JSONReader.existingUser("password"));
-	        verifyThatThoseProductsAreVisibleInCartAfterLoginAsWell(productsNames);
-	        verifyThatCartIsEmpty();
-	    }
+	                .fillCorrectLogin(email, password);
+	    verifyThatThoseProductsAreVisibleInCartAfterLoginAsWell(productsNames);
+	    verifyThatCartIsEmpty();
+	}
 
-	    @Step("Click 'Cart' button and verify that products are visible in cart")
-	    private void clickCartButtonAndVerifyThatProductsAreVisibleInCart( List<String> productsNames) {
-	        List<String> productsNamesAdded = new HomePage(getDriver())
-	                .cartButtonClick()
-	                .getProductsNames();
-	        for (int i = 0; i < productsNames.size(); i++) {
-	            Assert.assertEquals(productsNames.get(i), productsNamesAdded.get(i), "Verify that products are visible in cart");
-	            System.out.println("Search: " + productsNames.get(i) + " = Added: " + productsNamesAdded.get(i));
-	        }
+	private void clickCartButtonAndVerifyThatProductsAreVisibleInCart( List<String> productsNames) {
+		System.out.println("Step : Click Cart Button And Verify That Products Are Visible In Cart");
+	    List<String> productsNamesAdded = 
+	    			new HomePage(getDriver())
+	                		.cartButtonClick()
+	                		.getProductsNames();
+	    for (int i = 0; i < productsNames.size(); i++) {
+	    	Assert.assertEquals(productsNames.get(i), productsNamesAdded.get(i), "Verify that products are visible in cart");
+	        System.out.println("Search: " + productsNames.get(i) + " = Added: " + productsNamesAdded.get(i));
 	    }
+	    
+	}
 
-	    @Step("Verify that those products are visible in cart after login as well")
-	    private void verifyThatThoseProductsAreVisibleInCartAfterLoginAsWell( List<String> productsNames) {
-	        clickCartButtonAndVerifyThatProductsAreVisibleInCart(productsNames);
-	    }
+	private void verifyThatThoseProductsAreVisibleInCartAfterLoginAsWell( List<String> productsNames) {
+	    System.out.println("Verify That Those Products Are Visible In Cart After Login As Well");
+	    clickCartButtonAndVerifyThatProductsAreVisibleInCart(productsNames);
+	}
 
-	    @Step("Verify 'Cart is empty! Click here to buy products.' is visible")
-	    private void verifyThatCartIsEmpty() throws InterruptedException {
-	        String emptyCartText = new CartPage(getDriver())
-	                .deleteAllAddedProducts()
-	                .getEmptyCartSpan()
-	                .getText();
-	        Assert.assertEquals(emptyCartText, "Cart is empty! Click here to buy products.", "Verify 'Cart is empty! Click here to buy products.' is visible");
-	    }
+	private void verifyThatCartIsEmpty() throws InterruptedException {
+		System.out.println("Step : Verify That Cart Is Empty");
+	    String emptyCartText = 
+	    			new CartPage(getDriver())
+	                		.deleteAllAddedProducts()
+	                		.getEmptyCartSpan()
+	                		.getText();
+	    Assert.assertEquals(emptyCartText, "Cart is empty! Click here to buy products.", "Verify 'Cart is empty! Click here to buy products.' is visible");
+	}
 }
--- src/main/java/testCases/TestCase23.java
+++ src/main/java/testCases/TestCase23.java
@@ -1,21 +1,25 @@
 package testCases;
 
 import java.io.IOException;
-import org.json.simple.parser.ParseException;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
 
-import setup.AutomationExerciseConstants;
+import setup.TestDataReader;
 import setup.TestSetup;
 
 public class TestCase23 extends TestSetup{
 
-	String name = AutomationExerciseConstants.name;
-    String email = AutomationExerciseConstants.email;
     
     @Test
     public void verifyAddressDetailsInCheckoutPage() throws IOException, ParseException {
         TestCase1.isHomePageVisible();
+        
+		JSONObject commonTestDetails = TestDataReader.commonTestData();
+		String name=(String) commonTestDetails.get("name");
+		String email=(String) commonTestDetails.get("email");
+		
         TestCase14.checkIsAccountCreatedAndClickContinueButton(name, email);
         TestCase14.checkLoggedInAsUsernameAtTop(name);
         TestCase14.isCartPageDisplayed();
